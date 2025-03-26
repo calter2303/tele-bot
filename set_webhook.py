bbdbd070692147de5354087018168c3282119a5b
calter2303 <123729537+calter2303@users.noreply.github.com>
2025-03-26 23:11:16 +0800

    Update set_webhook.py

--- set_webhook.py
+++ set_webhook.py
@@ -1,11 +1,11 @@
 import requests
 import os
 
-WEBHOOK_URL = "https://tele-bot-production-8c1a.up.railway.app/" + os.getenv("TELEGRAM_BOT_TOKEN")  # Ganti dengan URL dari Railway
+WEBHOOK_URL = f"https://tele-bot-production-8c1a.up.railway.app/{os.getenv('TELEGRAM_BOT_TOKEN')}"  # URL webhook yang benar
 
 response = requests.post(
     f"https://api.telegram.org/bot{os.getenv('TELEGRAM_BOT_TOKEN')}/setWebhook",
     data={"url": WEBHOOK_URL}
 )
 
-print(response.text)
+print(response.text)  # Menampilkan hasil response dari Telegram API
