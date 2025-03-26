23bfcff0c5505390165e62cc53cb44b9ab0dd6b4
calter2303 <123729537+calter2303@users.noreply.github.com>
2025-03-26 17:28:14 +0800

    Update payment_service.py

--- payment_service.py
+++ payment_service.py
@@ -1,10 +1,19 @@
+import os
 import midtransclient
+
+# Ambil server_key dan client_key dari environment variables
+server_key = os.getenv("MIDTRANS_SERVER_KEY")
+client_key = os.getenv("MIDTRANS_CLIENT_KEY")
+
+# Pastikan key tersebut ada
+if not server_key or not client_key:
+    raise ValueError("MIDTRANS_SERVER_KEY or MIDTRANS_CLIENT_KEY is not set in the environment variables")
 
 # Inisialisasi Midtrans
 midtrans = midtransclient.CoreApi(
     is_production=False,  # Set ke True jika di lingkungan produksi
-    server_key="YOUR_SERVER_KEY",  # Gantilah dengan server key dari Midtrans
-    client_key="YOUR_CLIENT_KEY"   # Gantilah dengan client key dari Midtrans
+    server_key=server_key,  # Gantilah dengan server key dari Midtrans
+    client_key=client_key   # Gantilah dengan client key dari Midtrans
 )
 
 def create_payment_link(user, amount):
