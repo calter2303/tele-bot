66bc647f42d9ffad5d0bfab210b2bc2bda4d7b1a
calter2303 <123729537+calter2303@users.noreply.github.com>
2025-03-27 08:27:25 +0800

    Update payment_service.py

--- payment_service.py
+++ payment_service.py
@@ -5,7 +5,6 @@ import midtransclient
 # Memuat variabel lingkungan
 load_dotenv()
 
-# Ambil server_key dan client_key dari environment variables
 server_key = os.getenv("MIDTRANS_SERVER_KEY")
 client_key = os.getenv("MIDTRANS_CLIENT_KEY")
 
@@ -13,24 +12,24 @@ if not server_key or not client_key:
     raise ValueError("MIDTRANS_SERVER_KEY or MIDTRANS_CLIENT_KEY is not set in the environment variables")
 
 # Inisialisasi Midtrans
-midtrans = midtransclient.CoreApi(
+midtrans = midtransclient.Snap(
     is_production=False,
-    server_key=server_key,
-    client_key=client_key
+    server_key=server_key
 )
 
 def create_payment_link(user, amount):
     transaction_details = {
-        'order_id': f"order-{user.id}",
-        'gross_amount': amount
-    }
-
-    customer_details = {
-        'first_name': user.first_name
+        'transaction_details': {
+            'order_id': f"order-{user.id}",
+            'gross_amount': amount
+        },
+        'customer_details': {
+            'first_name': user.first_name
+        }
     }
 
     try:
-        transaction = midtrans.transaction.create(transaction_details, customer_details)
+        transaction = midtrans.create_transaction(transaction_details)
         return transaction['redirect_url']
     except Exception as e:
         print(f"Error creating payment link: {e}")
